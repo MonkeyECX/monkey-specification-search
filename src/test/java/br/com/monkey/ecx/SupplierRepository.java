e9c2fac8234f7b3e2f5210d8c9fd87d37772d6dd
brcarminatti <brcarminatti@gmail.com>
2021-03-13 07:12:47 -0300

    [SPK-110] Remove unused class.

--- src/test/java/br/com/monkey/ecx/SupplierRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.monkey.ecx;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-interface SupplierRepository
-		extends JpaRepository<Supplier, Integer>, JpaSpecificationExecutor<Supplier> {
-
-}
